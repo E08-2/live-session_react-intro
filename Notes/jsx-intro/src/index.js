28ffbef8f979e3615a5901f16501b169c8c19642
Jamie <jamie-cartwright@digitalcareerinstitute.org>
2022-03-10 11:48:50 +0100

    Re-add JSX intro notes

---
+++ Notes/jsx-intro/src/index.js
@@ -0,0 +1,41 @@
+// * Q1. How can we create a very basic UI in index.js using React?
+// * Q2. How can we "talk" to index.html, to display the UI we create using React?
+
+// * First, we will run "npm install" to install all the dependencies React needs.
+// Usually, we will not include the node_modules folder in GitHub repos, because it can be quite large.
+// Using "npm install" in a local repo will find all the dependencies the project needs (using package.json)...
+// ... and then install them all in your local repo!
+
+// =================================================
+
+// * 1. What is JSX?
+
+// JSX is a syntax extension of JavaScript.
+// It allows us to write "HTML-like" elements in React (using JS code), which can then be "translated" into normal HTML.
+// JSX is faster than regular JS
+// It allows us to put HTML in the DOM using JS without using document.createElement() or appendChild().
+
+// Import React so we can use JSX in "index.js".
+import React from "react";
+// Import ReactDOM to "translate" our JSX code into HTML and "give" it to "index.html"
+import ReactDOM from "react-dom";
+
+// Get a reference to the HTML "root" element
+const root = document.querySelector("#root");
+
+// Note: we can also use JS variables in our JSX
+// By putting the variable between curly braces { }
+let endOfSentence = "world!!";
+
+// Create some JSX and assign it to a variable
+let jsxDiv = (
+	<div>Hello {endOfSentence}</div>
+);
+
+// ReactDOM.render() is the "gateway" from index.js to index.html
+// It takes two arguments:
+// * 1. What JSX we want to render in HTML
+// * 2. Where we want to render it
+//              what    where
+ReactDOM.render(jsxDiv, root);
+// ReactDOM.render(jsxDiv, document.querySelector("#root"));
